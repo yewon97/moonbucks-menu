10a1aede887b9f1410005e5a1224e2a8c51d92eb
yewon97 <dpdnjs9936@gmail.com>
2022-01-14 14:47:50 +0900

    메뉴 목록 템플릿 innerHTML로 추가

--- src/js/index.js
+++ src/js/index.js
@@ -8,15 +8,33 @@ const $ = (selector) => document.querySelector(selector);
 
 function App() {
   // form 태그가 자동으로 전송되는걸 막아줌
-  $("#espresso-menu-form").addEventListener('submit', (e) => {
+  $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
   });
 
   // 메뉴의 이름을 입련받는건
   $('#espresso-menu-name').addEventListener('keypress', (e) => {
-    console.log(e.key);
     if (e.key === 'Enter') {
-      console.log(document.querySelector('#espresso-menu-name').value);
+      const espressoMenuName = $('#espresso-menu-name').value;
+      const menuItemTemplate = (espressoMenuName) => {
+        return `
+        <li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+        </li>`;
+      };
+      $("#espresso-menu-list").innerHTML = menuItemTemplate(espressoMenuName);
     }
   });
 }
