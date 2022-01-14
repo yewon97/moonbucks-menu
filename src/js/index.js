033d1e4d6cf005a7a4c189c40f52c6f14d8a11b0
yewon97 <dpdnjs9936@gmail.com>
2022-01-14 23:52:54 +0900

    this.currentCategory를 생성해서 현재 카테고리 위치 표현

--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,7 @@ function App() {
     teavana: [],
     desert: [],
   };
+  this.currentCategory = 'espresso'; // 처음 보여지는 카테고리는 무조건 espresso
 
   // App이라는 함수가 인스턴스로 생성이 될 때 로컬스토리지 안에 있는 것을 불러오면 좋을 것 같음
   // App이 생성될 때 실행을 하기 위해서 초기화 한다는 말의 init 메서드를 생성
@@ -32,7 +33,7 @@ function App() {
 
   // 데이터 그려주는 로직 (재사용)
   const render = () => {
-    const template = this.menu
+    const template = this.menu[this.currentCategory]
       .map((item, index) => {
         return `
       <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
@@ -68,7 +69,7 @@ function App() {
       return;
     }
     const espressoMenuName = $('#espresso-menu-name').value;
-    this.menu.push({ name: espressoMenuName });
+    this.menu[this.currentCategory].push({ name: espressoMenuName });
     store.setLocalStorage(this.menu);
     render();
     $('#espresso-menu-name').value = '';
