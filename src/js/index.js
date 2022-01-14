63d92e599a4880793920d3dbad49d1d61f9fd26f
yewon97 <dpdnjs9936@gmail.com>
2022-01-15 00:08:42 +0900

    render 함수에 품절 버튼 추가

--- src/js/index.js
+++ src/js/index.js
@@ -39,6 +39,12 @@ function App() {
       <li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2">
       <span class="w-100 pl-2 menu-name">${item.name}</span>
       <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+      > 
+      품절
+      </button>
+      <button
         type="button"
         class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
       >
